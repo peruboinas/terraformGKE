bd10b3787f26d340af9316143d27784c70c8743b
unknown <vijaykumar.peruboina@gmail.com>
2020-08-21 23:20:18 +0530

    test

---
+++ variable.tf
@@ -0,0 +1,13 @@
+variable "project_id" {
+  description = "Google Project ID."
+}
+variable "region" {
+  description = "Google Cloud region"
+  default     = "europe-west2"
+}
+
+variable "machine_type" {
+  description = "Google VM Instance type."
+}
+
+
