e4d067155afa57c91e2f3445da383492f1b03ed1
peruboinas <58944139+peruboinas@users.noreply.github.com>
2020-08-21 23:45:52 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,13 +1,13 @@
-variable "project_id" {
-  description = "Google Project ID."
+module "network" {
+  source = "git@github.com:FairwindsOps/terraform-gcp-vpc-native.git//default?ref=default-v2.1.0"
+  // base network parameters
+  network_name     = "kube"
+  subnetwork_name  = "kube-subnet"
+  region           = "us-central1"
+  enable_flow_logs = "false"
+  // subnetwork primary and secondary CIDRS for IP aliasing
+  subnetwork_range    = "10.40.0.0/16"
+  subnetwork_pods     = "10.41.0.0/16"
+  subnetwork_services = "10.42.0.0/16"
 }
-variable "region" {
-  description = "Google Cloud region"
-  default     = "europe-west2"
-}
-
-variable "machine_type" {
-  description = "Google VM Instance type."
-}
-
 
